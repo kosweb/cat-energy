a89f24f8f3b1c2367198cc8f5688e07fa4457393
kosweb <koleitdev@gmail.com>
2020-07-08 00:38:39 +0300

    some fix

--- source/js/cat-slider.js
+++ source/js/cat-slider.js
@@ -3,6 +3,10 @@ var catAfter = document.querySelector('.example__after');
 var buttonBefore = document.querySelector('.example__option--before');
 var buttonAfter = document.querySelector('.example__option--after');
 
+function rangeSlider(value) {
+	document.querySelector('.example__before').style.width = +value+"%";
+};
+
 buttonBefore.addEventListener('click', function(evt) {
 	if (catAfter.classList.contains('example__cat--show')) {
 		catAfter.classList.remove('example__cat--show');
@@ -16,7 +20,3 @@ buttonAfter.addEventListener('click', function(evt) {
 		catAfter.classList.add('example__cat--show');
 	}
 });
-
-function rangeSlider(value) {
-	document.querySelector('.example__before').style.width = +value+"%";
-};
